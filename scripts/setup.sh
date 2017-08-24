23aaa49399b0c85e0cb4bd9aa141d03963bcd018
Marco Bonetti <mbonetti@gmail.com>
2017-08-24 11:54:55 +0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export DOCKER_IMG="cruncher/postgresql:latest"
+export DOCKER_IMG="cruncher/postgresql:9.6-2-wale"
 mkdir -p /var/db
 apt-get -qq -y install docker.io
 docker pull $DOCKER_IMG
