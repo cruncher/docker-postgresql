bee1dfb3d50325583323bdfbc05526d7396ae617
Marco Bonetti <marco@cruncher.ch>
2020-06-08 11:26:13 +0200

    dont trust by default

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export DOCKER_IMG="cruncher/postgresql:12.beta-wale"
+export DOCKER_IMG="cruncher/postgresql:11.6-wale"
 mkdir -p /var/db
 # apt-get -qq -y install docker.io
 # docker pull $DOCKER_IMG
