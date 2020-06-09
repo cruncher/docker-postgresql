93ca20afc8677a1904180efc6eb136b03d07c14f
Marco Bonetti <marco@cruncher.ch>
2020-06-09 08:57:20 +0200

    11.6-wale.r2: dont trust samenet by default, but fallback to trusting samehost in this case

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export DOCKER_IMG="cruncher/postgresql:11.6-wale"
+export DOCKER_IMG="cruncher/postgresql:11.6-wale.r2"
 mkdir -p /var/db
 # apt-get -qq -y install docker.io
 # docker pull $DOCKER_IMG
